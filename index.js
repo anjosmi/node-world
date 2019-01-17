6701b99e21896223d82a92dfd98a1f4cd4546611
Michele Anjos <anjos.mi@gmail.com>
2019-01-17 13:56:52 -0500

    creating a new route example

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get('/', (req, res) => {
     res.send('Hello!!!!');
 });
 
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening on port ...' + port));
